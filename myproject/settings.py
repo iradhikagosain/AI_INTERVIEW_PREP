4fb572b9cc0fae1538073adb36a50e08bda22d91
Radhika Gosain <iradhikagosain@gmail.com>
2025-07-16 16:42:32 +0530

    Resolve conflicts

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j9^fshbjg3iw8(k@i)b97pp*1oh$us*k6jp^_v-_x2pg9z25#n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
